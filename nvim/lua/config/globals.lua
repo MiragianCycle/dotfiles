290c4b367aa1b5cb739a8c515b0e56d9be6ce0d0
Theena Kumaragurunathan <theenat.k@gmail.com>
2024-08-14 19:37:25 +0530

    Updated NeoVim dotfiles

--- nvim/lua/config/globals.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " " 
-vim.g.languagetool_server_command = '/opt/homebrew/bin/languagetool'
-
--- nvim/lua/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  }
-
-
